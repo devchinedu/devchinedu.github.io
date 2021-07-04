bd751c42ddac3b8f69d11804db281b46c83db3db
devchinedu <c.daviduchenna@gmail.com>
2021-07-04 20:12:04 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,66 @@
 'use strict';
 
-const aboutBtn = document.querySelector('.aboutBtn');
-let about = document.querySelector('#about');
+const aboutBtn = document.querySelectorAll('.aboutBtn');
+const resumeBtn = document.querySelectorAll('.resume');
+const portofolioBtn = document.querySelectorAll('.portofolio');
+const contactBtn = document.querySelectorAll('.contact');
+
+const removeHeader = function(){
+    document.querySelector('header').style.display = 'none';
+}
+
+const removeFlex = function(){
+    if(
+        document.querySelector('#about').style.display = 'flex',
+        document.querySelector('#resume').style.display = 'flex',
+        document.querySelector('.gridContainer').style.display = 'flex',
+        document.querySelector('#contact').style.display = 'flex'
+    ){
+        document.querySelector('#about').style.display = 'none',
+        document.querySelector('#resume').style.display = 'none',
+        document.querySelector('.gridContainer').style.display = 'none',
+        document.querySelector('#contact').style.display = 'none'
+    }
+}
 
 
-aboutBtn.addEventListener('click', function(){
-console.log('clic');
-about.style.display ='flex';
-document.querySelector('header').style.display = 'none';
-    
+
+
+
+
+
+  aboutBtn.forEach(function(i){
+      i.addEventListener('click', function(){
+        removeHeader();
+        removeFlex();
+        document.querySelector('#about').style.display = 'flex';
+      })
+  })
+
+
+  resumeBtn.forEach(function(i){
+    i.addEventListener('click', function(){
+    removeHeader();
+    removeFlex();
+    document.querySelector('#resume').style.display = 'flex';
+
+})
 })
 
+portofolioBtn.forEach(function(i){
+    i.addEventListener('click', function(){
+    removeHeader();
+    removeFlex();
+    document.querySelector('.gridContainer').style.display = 'flex';
+   
+})
+})
+
+
+contactBtn.forEach(function(i){
+    i.addEventListener('click', function(){
+    removeHeader();
+    removeFlex();
+    document.querySelector('#contact').style.display = 'flex';
+})
+})
